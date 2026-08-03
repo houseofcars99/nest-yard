f8f1bf05b2e656149370cf0fd271a9c68bcc7002
houseofcars99 <houseofcars.pl@gmail.com>
2026-08-04 01:37:24 +0200

    Show new logo in admin panel

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,12 +1,14 @@
 import "./admin-enhancements.css";
 import { AdminClient } from "@/components/AdminClient";
 import { AdminEnhancements } from "@/components/AdminEnhancements";
+import { AdminBrandLogo } from "@/components/AdminBrandLogo";
 
 export default function AdminPage() {
   return (
     <>
       <AdminClient />
       <AdminEnhancements />
+      <AdminBrandLogo />
     </>
   );
 }
