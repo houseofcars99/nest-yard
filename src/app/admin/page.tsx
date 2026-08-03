5ef115b9a5d00ad3d4c95a09af247276eb395bfa
houseofcars99 <houseofcars.pl@gmail.com>
2026-08-04 01:20:31 +0200

    Enable admin printing and image upload enhancements

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,5 +1,12 @@
+import "./admin-enhancements.css";
 import { AdminClient } from "@/components/AdminClient";
+import { AdminEnhancements } from "@/components/AdminEnhancements";
 
 export default function AdminPage() {
-  return <AdminClient />;
+  return (
+    <>
+      <AdminClient />
+      <AdminEnhancements />
+    </>
+  );
 }
